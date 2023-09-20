75b73928999e5fcddf623194f07d9842f90f4aff
valen1079534 <valendiazlerma16@gmail.com>
2023-09-20 16:47:47 -0500

    cambios 39

---
+++ js/mustang.js
@@ -0,0 +1,36 @@
+let boxSlider = document.getElementById("boxSlider");
+let iconChevronRight = document.getElementById("iconChevronRight")
+let iconChevronLeft = document.getElementById("iconChevronLeft")
+
+iconChevronRight.addEventListener("click", function()
+{
+    if(boxSlider.classList.contains("box-slider-1")){
+    boxSlider.classList.remove("box-slider-1");
+    boxSlider.classList.add("box-slider-2");
+    }
+    else if(boxSlider.classList.contains("box-slider-2")){
+        boxSlider.classList.remove("box-slider-2");
+        boxSlider.classList.add("box-slider-3");
+    }
+    else if(boxSlider.classList.contains("box-slider-3")){
+        boxSlider.classList.remove("box-slider-3");
+        boxSlider.classList.add("box-slider-4");
+    }
+})
+
+iconChevronLeft.addEventListener("click", function()
+{
+    if(boxSlider.classList.contains("box-slider-4")){
+    boxSlider.classList.remove("box-slider-4");
+    boxSlider.classList.add("box-slider-3");
+    }
+    else if(boxSlider.classList.contains("box-slider-3")){
+        boxSlider.classList.remove("box-slider-3");
+        boxSlider.classList.add("box-slider-2");
+    }
+    else if(boxSlider.classList.contains("box-slider-2")){
+        boxSlider.classList.remove("box-slider-2");
+        boxSlider.classList.add("box-slider-1");
+    }
+
+})
