86928c35a5e2a7fa27426ed0c78570922939a446
valen1079534 <valendiazlerma16@gmail.com>
2023-09-27 16:45:51 -0500

    New slider

--- js/mustang.js
+++ js/mustang.js
@@ -1,36 +1,52 @@
 let boxSlider = document.getElementById("boxSlider");
 let iconChevronRight = document.getElementById("iconChevronRight")
 let iconChevronLeft = document.getElementById("iconChevronLeft")
+let signal1 = document.getElementById("signal1")
+let signal2 = document.getElementById("signal2")
+let signal3 = document.getElementById("signal3")
+let signal4 = document.getElementById("signal4")
 
 iconChevronRight.addEventListener("click", function()
 {
     if(boxSlider.classList.contains("box-slider-1")){
-    boxSlider.classList.remove("box-slider-1");
-    boxSlider.classList.add("box-slider-2");
+    boxSlider.classList.remove("box-slider-1")
+    boxSlider.classList.add("box-slider-2")
+    signal1.classList.remove("signal-select")
+    signal2.classList.add("signal-select")
     }
     else if(boxSlider.classList.contains("box-slider-2")){
-        boxSlider.classList.remove("box-slider-2");
-        boxSlider.classList.add("box-slider-3");
+        boxSlider.classList.remove("box-slider-2")
+        boxSlider.classList.add("box-slider-3")
+        signal2.classList.remove("signal-select")
+        signal3.classList.add("signal-select")
     }
     else if(boxSlider.classList.contains("box-slider-3")){
-        boxSlider.classList.remove("box-slider-3");
-        boxSlider.classList.add("box-slider-4");
+        boxSlider.classList.remove("box-slider-3")
+        boxSlider.classList.add("box-slider-4")
+        signal3.classList.remove("signal-select")
+        signal4.classList.add("signal-select")
     }
 })
 
 iconChevronLeft.addEventListener("click", function()
 {
     if(boxSlider.classList.contains("box-slider-4")){
-    boxSlider.classList.remove("box-slider-4");
-    boxSlider.classList.add("box-slider-3");
+    boxSlider.classList.remove("box-slider-4")
+    boxSlider.classList.add("box-slider-3")
+    signal4.classList.remove("signal-select")
+    signal3.classList.add("signal-select")
     }
     else if(boxSlider.classList.contains("box-slider-3")){
-        boxSlider.classList.remove("box-slider-3");
-        boxSlider.classList.add("box-slider-2");
+        boxSlider.classList.remove("box-slider-3")
+        boxSlider.classList.add("box-slider-2")
+        signal3.classList.remove("signal-select")
+        signal2.classList.add("signal-select")
     }
     else if(boxSlider.classList.contains("box-slider-2")){
-        boxSlider.classList.remove("box-slider-2");
-        boxSlider.classList.add("box-slider-1");
+        boxSlider.classList.remove("box-slider-2")
+        boxSlider.classList.add("box-slider-1")
+        signal2.classList.remove("signal-select")
+        signal1.classList.add("signal-select")
     }
 
 })
