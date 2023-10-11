ef399c7c60abda43b28f436f3ad3eaf6cc203547
valen1079534 <valendiazlerma16@gmail.com>
2023-10-11 16:38:02 -0500

    Carrito

--- js/carrito.js
+++ js/carrito.js
@@ -1,20 +1,25 @@
 let boxMinus = document.getElementById("boxMinus")
 let boxCan = document.getElementById("boxCan")
 let boxPlus = document.getElementById("boxPlus")
-let boxCantidadCop = document.getElementById("boxCantidadCop")
-let subTotal = document.getElementById("subTotal")
+let numeroProductos = document.getElementById("numeroProductos")
+let productos = document.getElementById("Productos")
+let boxCantidadCop = 20000
+let auxBoxCantidadCop = new Intl.NumberFormat("de-DE").format(boxCantidadCop)
+document.getElementById("boxCantidaCop").innerHTML = auxBoxCantidadCop
 
 
 boxPlus.addEventListener("click", function() {
 let auxCan = boxCan.innerHTML
 
-
     if(auxCan < 10){
         auxCan++;
         boxCan.innerHTML = auxCan
-    }
-
-       
+        numeroProductos.innerHTML = auxCan
+        
+        determinarPs(auxCan)
+        calcularSubtotal()
+        
+    }   
 })
 
 
@@ -23,8 +28,34 @@ boxMinus.addEventListener("click", function(){
     if(auxCan > 0){
         auxCan--;
         boxCan.innerHTML = auxCan
+        numeroProductos.innerHTML = auxCan
+
+        determinarPs(auxCan)
+        calcularSubtotal()
     }
 })
+
+function calcularSubtotal(){
+    let auxCan = boxCantidadCop.innerHTML
+    let subTotal = document.getElementById("subTotal").innerHTML = boxCantidadCop * Number(boxCan.innerHTML)
+
+    let auxSubTotal = new Intl.NumberFormat("de-DE").format(subTotal)
+    document.getElementById("subTotal").innerHTML = auxSubTotal
+
+   
+    }
+
+function determinarPs(auxCan){
+    if((auxCan) == 0 || auxCan > 1){
+        productos.innerHTML = "productos"
+    }else if(auxCan == 1){
+        productos.innerHTML = "producto"
+    }
+}
+    
+    
+
+
 
 
 
