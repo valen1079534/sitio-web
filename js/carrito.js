462ec887389bff6bcb67ce9a9baac3297a6ad8f4
valen1079534 <valendiazlerma16@gmail.com>
2023-10-11 12:59:28 -0500

    holaaaa

--- js/carrito.js
+++ js/carrito.js
@@ -7,16 +7,13 @@ let subTotal = document.getElementById("subTotal")
 
 boxPlus.addEventListener("click", function() {
 let auxCan = boxCan.innerHTML
-let boxCantidadCop = boxCantidadCop.innerHTML
-let subTotal = subTotal.innerHTML
 
-boxCantidadCop = 2
 
     if(auxCan < 10){
         auxCan++;
         boxCan.innerHTML = auxCan
     }
-    total = boxCantidadCop * subTotal.innerHTML
+
        
 })
 
