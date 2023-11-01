03a02774aae3b4b2e5a7844465ca10680f1f3f59
valen1079534 <valendiazlerma16@gmail.com>
2023-11-01 10:54:13 -0500

    Amazon

---
+++ js/pcAmazon.js
@@ -0,0 +1,6 @@
+let imagen1 = document.getElementById("imagen1");
+let imagen2 = document.getElementById("imagen2");
+let imagen3 = document.getElementById("imagen3");
+let imagen4 = document.getElementById("inagen4")
+let imagen5 = document.getElementById("imagen5")
+
