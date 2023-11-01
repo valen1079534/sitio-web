d230b79a613b6b96f1c109cbef5ae96138b60873
valen1079534 <valendiazlerma16@gmail.com>
2023-11-01 16:46:56 -0500

    jdjfjd

--- js/pcAmazon.js
+++ js/pcAmazon.js
@@ -1,6 +1,51 @@
-let imagen1 = document.getElementById("imagen1");
-let imagen2 = document.getElementById("imagen2");
-let imagen3 = document.getElementById("imagen3");
-let imagen4 = document.getElementById("inagen4")
+let imagen = document.getElementById("imagen")
+let imagen1 = document.getElementById("imagen1")
+let imagen2 = document.getElementById("imagen2")
+let imagen3 = document.getElementById("imagen3")
+let imagen4 = document.getElementById("imagen4")
 let imagen5 = document.getElementById("imagen5")
 
+/* window.addEventListener('load', iniciar, false)
+
+function iniciar(){
+    var imagen = document.getElementById("imagen")
+    imagen.addEventListener("mouseover",peligro, false)
+}
+function peligro(){
+    document.getElementById("imagen").src = "https://m.media-amazon.com/images/I/61YiMdxbvjL._AC_SY535_.jpg"
+    document.getElementById("imagen").src = "https://m.media-amazon.com/images/I/61UuwHvTwoL._AC_SY535_.jpg"
+}
+  */
+ 
+
+
+
+imagen1.addEventListener("click", function(){
+    imagen.src = ' '
+    imagen.src = imagen1.src
+}) 
+
+
+imagen2.addEventListener("click", function(){
+    imagen.src = ' '
+    imagen.src = imagen2.src
+}) 
+ 
+
+imagen3.addEventListener("click", function(){
+    imagen.src = ' '
+    imagen.src = imagen3.src
+}) 
+
+imagen4.addEventListener("click", function(){
+    imagen.src = ' '
+    imagen.src = imagen4.src
+}) 
+
+imagen5.addEventListener("click", function(){
+    imagen.src = ' '
+    imagen.src = imagen5.src
+}) 
+  
+ 
+ 
