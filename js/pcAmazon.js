a3e20782e43b94eb5abca4085a0b3a95d29e48a6
valen1079534 <valendiazlerma16@gmail.com>
2023-11-15 16:38:05 -0500

    sigue la estrella de tu vida

--- js/pcAmazon.js
+++ js/pcAmazon.js
@@ -1,109 +1,116 @@
 let imagen = document.getElementById("imagen")
-let imagenPrincipal = document.getElementById("imagenPrincipal")
 let imagen1 = document.getElementById("imagen1")
 let imagen2 = document.getElementById("imagen2")
 let imagen3 = document.getElementById("imagen3")
 let imagen4 = document.getElementById("imagen4")
 let imagen5 = document.getElementById("imagen5")
-let iconExmark = document.getElementById("iconExmark")
-let modal = document.getElementById("modal")
-let mainContent = document.getElementById("mainContent")
-/* 
-let imagen11 = document.getElementById("imagen11")
-let imagen22 = document.getElementById("imagen22")
-let imagen33 = document.getElementById("imagen33")
-let imagen44 = document.getElementById("imagen44")
-let imagen55 = document.getElementById("imagen55") */
+let close = document.getElementById("close")
+let modal = document.getElementById("box-modal")
+let imgPrincipal = document.getElementById("imagePrincipal")
+let principal1 = document.getElementById("principal1")
+let principal2 = document.getElementById("principal2")
+let principal3 = document.getElementById("principal3")
+let principal4 = document.getElementById("principal4")
+let principal5 = document.getElementById("principal5")
 
 
-/* window.addEventListener('load', iniciar, false)
-
-function iniciar(){
-    var imagen = document.getElementById("imagen")
-    imagen.addEventListener("mouseover",peligro, false)
+function setActive(principal){
+    var imagen = document.querySelectorAll(".imgPrinReducidas")
+    imagen.forEach(function(imagePrin){
+        imagePrin.classList.remove('activation')
+    })
+    principal.classList.add('activation')
 }
-function peligro(){
-    document.getElementById("imagen").src = "https://m.media-amazon.com/images/I/61YiMdxbvjL._AC_SY535_.jpg"
-    document.getElementById("imagen").src = "https://m.media-amazon.com/images/I/61UuwHvTwoL._AC_SY535_.jpg"
-}
-  */
+
+
+principal1.addEventListener("mouseover", function(){
+    imgPrincipal.src = ' '
+    imgPrincipal.src = principal1.src
+    setActive(principal1)
+}) 
+
+
+principal2.addEventListener("mouseover", function(){
+    imgPrincipal.src = ' '
+    imgPrincipal.src = principal2.src
+    setActive(principal2)
+}) 
  
 
+principal3.addEventListener("mouseover", function(){
+    imgPrincipal.src = ' '
+    imgPrincipal.src = principal3.src
+    setActive(principal3)
+}) 
+
+principal4.addEventListener("mouseover", function(){
+    imgPrincipal.src = ' '
+    imgPrincipal.src = principal4.src
+    setActive(principal4)
+}) 
+
+principal5.addEventListener("mouseover", function(){
+    imgPrincipal.src = ' '
+    imgPrincipal.src = principal5.src
+    setActive(principal5)
+}) 
 
 
+
+//cabio de color
+function setActive(img){
+    var hola = document.querySelectorAll(".imgReducidas")
+    hola.forEach(function(image){
+        image.classList.remove('active')
+    })
+    img.classList.add('active')
+}
+
+//cambio de imagen
 imagen1.addEventListener("click", function(){
     imagen.src = ' '
     imagen.src = imagen1.src
+    //para que se ejecute el cambio de color en la imagen
+    setActive(imagen1)
 }) 
 
 
 imagen2.addEventListener("click", function(){
     imagen.src = ' '
     imagen.src = imagen2.src
+    setActive(imagen2)
 }) 
  
 
 imagen3.addEventListener("click", function(){
     imagen.src = ' '
     imagen.src = imagen3.src
+    setActive(imagen3)
 }) 
 
 imagen4.addEventListener("click", function(){
     imagen.src = ' '
     imagen.src = imagen4.src
+    setActive(imagen4)
 }) 
 
 imagen5.addEventListener("click", function(){
     imagen.src = ' '
     imagen.src = imagen5.src
+    setActive(imagen5)
 }) 
 
-iconExmark.addEventListener("click", function(){
-    
-})
 
 
-function cerrarse(){
-    var div = document.getElementById("modal")
-    div.style.display = "none"
+function cerrar() {
+    modal.classList.remove("visible")
+}
+
+function abrir(){
+    modal.classList.add("visible")
 }
 
 
-/* function abrir(){
-    document.getElementById("mainContent")
-    modal.classList.visible
-} */
 
 
-/* function abrir(){
-     /* document.getElementById("mainContent") *oeeeee")
-
-
-/* imagen11.addEventListener("click", function(){
-    imagenPrincipal.src = ' '
-    imagenPrincipal.src = imagen11.src
-}) 
-
-
-imagen22.addEventListener("click", function(){
-    imagenPrincipal.src = ' '
-    imagenPrincipal.src = imagen22.src
-}) 
  
-
-imagen33.addEventListener("click", function(){
-    imagenPrincipal.src = ' '
-    imagenPrincipal.src = imagen33.src
-}) 
-
-imagen44.addEventListener("click", function(){
-    imagenPrincipal.src = ' '
-    imagenPrincipal.src = imagen44.src
-}) 
-
-imagen55.addEventListener("click", function(){
-    imagenPrincipal.src = ' '
-    imagenPrincipal.src = imagen55.src
-}) 
- 
- */
