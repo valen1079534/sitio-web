e6ca3486bc3048a4be09871db23dfbd1b75a99c8
valen1079534 <valendiazlerma16@gmail.com>
2023-11-22 16:40:09 -0500

    hellooooooooooo

---
+++ js/Estrellas.js
@@ -0,0 +1,43 @@
+let input1 = document.getElementById("input1")
+let input2 = document.getElementById("input2")
+let input3 = document.getElementById("input3")
+let input4 = document.getElementById("input4")
+let input5 = document.getElementById("input5")
+
+
+
+let cargar = document.getElementById("cargar")
+
+let caja1 = document.getElementById("caja1")
+let caja2 = document.getElementById("caja2")
+let caja3 = document.getElementById("caja3")
+let caja4 = document.getElementById("caja4")
+let caja5 = document.getElementById("caja5")
+
+let resultado;
+
+cargar.addEventListener("click", () => {
+
+    let porcentaje = document.getElementById("result")
+    
+   resultado = parseInt(input5.value) + parseInt(input4.value) + parseInt(input3.value) + parseInt(input2.value) + parseInt(input1.value)
+
+   porcentaje.innerHTML = resultado
+
+   let total1= parseInt(input1.value) * 100 / resultado
+   caja1.style.width= `${total1}%`
+   
+   let total2 = parseInt(input2.value) * 100 / resultado
+   caja2.style.width= `${total2}%`
+    
+   let total3 = parseInt(input3.value) * 100 / resultado
+   caja3.style.width= `${total3}%`
+
+   let total4 = parseInt(input4.value) * 100 / resultado
+   caja4.style.width= `${total4}%`
+
+   let total5 = parseInt(input5.value) * 100 / resultado
+   caja5.style.width= `${total5}%`
+
+
+})
--- js/pcAmazon.js
+++ js/pcAmazon.js
@@ -1,71 +1,60 @@
+//imagenes reducidas encontradas en la pagina siguiente
 let imagen = document.getElementById("imagen")
 let imagen1 = document.getElementById("imagen1")
 let imagen2 = document.getElementById("imagen2")
 let imagen3 = document.getElementById("imagen3")
 let imagen4 = document.getElementById("imagen4")
 let imagen5 = document.getElementById("imagen5")
-let close = document.getElementById("close")
+
+//para abrir la pagina siguiente
 let modal = document.getElementById("box-modal")
+
+//imagenes pagina principal y su funcionalidad
 let imgPrincipal = document.getElementById("imagePrincipal")
 let principal1 = document.getElementById("principal1")
 let principal2 = document.getElementById("principal2")
 let principal3 = document.getElementById("principal3")
 let principal4 = document.getElementById("principal4")
 let principal5 = document.getElementById("principal5")
+
+//estrellas
 let star1 = document.getElementById("star1")
 let star2 = document.getElementById("star2")
 let star3 = document.getElementById("star3")
 let star4 = document.getElementById("star4")
 let star5 = document.getElementById("star5")
+//para manipular dese aca la puntucion 
+let puntuacion = document.getElementById("puntuacion").innerText = 2.5
 
-let buttonMarcar = document.getElementById("buttonMarcar")
-
-
-function setActive(principal){
-    var imagen = document.querySelectorAll(".imgPrinReducidas")
-    imagen.forEach(function(imagePrin){
-        imagePrin.classList.remove('activation')
-    })
-    principal.classList.add('activation')
-}
 
 //cambio de imagen solo con pasar el cursor 
-
 principal1.addEventListener("mouseover", function(){
     imgPrincipal.src = ' '
     imgPrincipal.src = principal1.src
     setActive(principal1)
 }) 
-
-
 principal2.addEventListener("mouseover", function(){
     imgPrincipal.src = ' '
     imgPrincipal.src = principal2.src
     setActive(principal2)
 }) 
- 
-
 principal3.addEventListener("mouseover", function(){
     imgPrincipal.src = ' '
     imgPrincipal.src = principal3.src
     setActive(principal3)
 }) 
-
 principal4.addEventListener("mouseover", function(){
     imgPrincipal.src = ' '
     imgPrincipal.src = principal4.src
     setActive(principal4)
 }) 
-
 principal5.addEventListener("mouseover", function(){
     imgPrincipal.src = ' '
     imgPrincipal.src = principal5.src
     setActive(principal5)
 }) 
 
-
-
-//cabio de color
+//cabio de color de las imagenes reducidas, que se encuentran en la pagina mas pewue
 function setActive(img){
     var hola = document.querySelectorAll(".imgReducidas")
     hola.forEach(function(image){
@@ -75,34 +64,27 @@ function setActive(img){
 }
 
 //cambio de imagen dando click
-
 imagen1.addEventListener("click", function(){
     imagen.src = ' '
     imagen.src = imagen1.src
     //para que se ejecute el cambio de color en la imagen
     setActive(imagen1)
 }) 
-
-
 imagen2.addEventListener("click", function(){
     imagen.src = ' '
     imagen.src = imagen2.src
     setActive(imagen2)
 }) 
- 
-
 imagen3.addEventListener("click", function(){
     imagen.src = ' '
     imagen.src = imagen3.src
     setActive(imagen3)
 }) 
-
 imagen4.addEventListener("click", function(){
     imagen.src = ' '
     imagen.src = imagen4.src
     setActive(imagen4)
 }) 
-
 imagen5.addEventListener("click", function(){
     imagen.src = ' '
     imagen.src = imagen5.src
@@ -119,14 +101,8 @@ function abrir(){
     modal.classList.add("visible")
 }
 
-
 //start
 //Para elaborar estrellas de porcentajes 
-
-buttonMarcar.addEventListener("click", () => {
-
-    let puntuacion = document.getElementById("puntuacion").value 
-
     if (puntuacion >= 0 && puntuacion <=0.2){
         star1.setAttribute("class", "fa-regular fa-star")
         star2.setAttribute("class", "fa-regular fa-star")
@@ -215,7 +191,9 @@ buttonMarcar.addEventListener("click", () => {
         star5.setAttribute("class", "fa-solid fa-star")
     }
 
-})
+    //barra de porcentaje 
+
+
 
 
 
