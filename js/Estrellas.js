39c03e6b852ffa13611faa97dc7bf65a22e90a6a
valen1079534 <valendiazlerma16@gmail.com>
2023-11-29 16:41:27 -0500

    holaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- js/Estrellas.js
+++ js/Estrellas.js
@@ -1,8 +1,8 @@
-/* let input1 = document.getElementById("input1")
+let input1 = document.getElementById("input1")
 let input2 = document.getElementById("input2")
 let input3 = document.getElementById("input3")
 let input4 = document.getElementById("input4")
-let input5 = document.getElementById("input5") */
+let input5 = document.getElementById("input5")
 
 let cargar = document.getElementById("cargar")
 
@@ -14,8 +14,37 @@ let caja5 = document.getElementById("caja5")
 
 let resultado;
 
+cargar.addEventListener("click", () => {
+
+    let porcentaje = document.getElementById("result")
+    
+   resultado = parseInt(input5.value) + parseInt(input4.value) + parseInt(input3.value) + parseInt(input2.value) + parseInt(input1.value)
+
+   porcentaje.innerHTML = resultado
+
+   let total1= parseInt(input1.value) * 100 / resultado
+   caja1.style.width= `${total1}%`
+   
+   let total2 = parseInt(input2.value) * 100 / resultado
+   caja2.style.width= `${total2}%`
+    
+   let total3 = parseInt(input3.value) * 100 / resultado
+   caja3.style.width= `${total3}%`
+
+   let total4 = parseInt(input4.value) * 100 / resultado
+   caja4.style.width= `${total4}%`
+
+   let total5 = parseInt(input5.value) * 100 / resultado
+   caja5.style.width= `${total5}%`
+
+    })
+
+
+
+
+
 //estrellas
-let star1 = document.getElementById("star1")
+/* let star1 = document.getElementById("star1")
 let star2 = document.getElementById("star2")
 let star3 = document.getElementById("star3")
 let star4 = document.getElementById("star4")
@@ -119,30 +148,7 @@ const input3 = 50
 const input4 = 29
 const input5 =10
 
+ */
 
-cargar.addEventListener("click", () => {
-
-    let porcentaje = document.getElementById("result")
-    
-   resultado = parseInt(input5) + parseInt(input4) + parseInt(input3) + parseInt(input2) + parseInt(input1)
-
-   porcentaje.innerHTML = resultado
-
-   let total1= parseInt(input1) * 100 / resultado
-   caja1.style.width= `${total1}%`
-   
-   let total2 = parseInt(input2) * 100 / resultado
-   caja2.style.width= `${total2}%`
-    
-   let total3 = parseInt(input3) * 100 / resultado
-   caja3.style.width= `${total3}%`
-
-   let total4 = parseInt(input4) * 100 / resultado
-   caja4.style.width= `${total4}%`
-
-   let total5 = parseInt(input5) * 100 / resultado
-   caja5.style.width= `${total5}%`
-
-    })
 
  
