6dd06992afa4472e5fef62215730da7c859565c3
valen1079534 <valendiazlerma16@gmail.com>
2023-12-13 16:38:11 -0500

    amazonNew

--- js/pcAmazon.js
+++ js/pcAmazon.js
@@ -24,7 +24,7 @@ let star3 = document.getElementById("star3")
 let star4 = document.getElementById("star4")
 let star5 = document.getElementById("star5")
 //para manipular dese aca la puntucion 
-let puntuacion = document.getElementById("puntuacion").innerText = 2.5
+let puntuacion = document.getElementById("puntuacion").innerText = 4.5
 
 
 //cambio de imagen solo con pasar el cursor 
@@ -191,7 +191,61 @@ function abrir(){
         star5.setAttribute("class", "fa-solid fa-star")
     }
 
-  
+    //
+    let start = document.getElementById("start")
+    let cajaBox = document.getElementById("caja-box")
+
+    let caja1 = document.getElementById("caja1")
+    let caja2 = document.getElementById("caja2")
+    let caja3 = document.getElementById("caja3")
+    let caja4 = document.getElementById("caja4")
+    let caja5 = document.getElementById("caja5")
+    let resultado2;
+
+    const input1 = 100
+    const input2 = 5
+    const input3 = 3
+    const input4 = 6
+    const input5 =1
+
+    start.addEventListener("mouseover", () =>{   
+        cargar()
+    })
+
+    start.addEventListener("mouseout", () => {
+
+        setTimeout(() =>{
+            cajaBox.style.display = "none"
+        }, 3000)
+    })
+      
+    function cargar() {
+        cajaBox.style.display = "inline"
+        setTimeout(() =>{
+            let porcentaje = document.getElementById("result")
+    
+            resultado2 = parseInt(input1) + parseInt(input2) + parseInt(input3) + parseInt(input4) + parseInt(input5)
+            porcentaje.innerHTML = resultado2
+    
+            let total1 = parseInt(input1) * 100 / resultado2
+            caja1.style.width= `${total1}%`
+    
+            let total2 = parseInt(input2) * 100 / resultado2
+            caja2.style.width = `${total2}%`
+    
+            let total3 = parseInt(input3) * 100 / resultado2
+            caja3.style.width = `${total3}%`
+    
+            let total4 = parseInt(input4) * 100 / resultado2
+            caja4.style.width = `${total4}%`
+    
+            let total5 = parseInt(input5) * 100 / resultado2
+            caja5.style.width = `${total5}%`
+        },1)
+
+    }
+
+    
 
 
 
